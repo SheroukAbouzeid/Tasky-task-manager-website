8b5225bd734f3f3af21532840e6bd6bdc55c89e9
Dina Ashraf <dinaashraf20003@gmail.com>
2024-09-26 16:59:48 +0300

    Navbar_Updated

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,99 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledNavbar = styled.div`
+  display: flex;
+  align-items: center;       
+  justify-content: space-between; 
+  background-color: #000000;
+  padding: 15px 20px;       
+  img {
+    width: 16%; 
+    height: auto; 
+  }
+`;
+
+const Nav = styled.div`
+  display: flex;
+  align-items: center;      
+  
+  h2 {
+    color: #ffffff;         
+    margin: 0 20px;       
+    transition: color 0.3s; 
+    font-size: 18px;
+
+    @media (min-width: 768px) {
+      font-size: 24px; 
+    }
+    @media (min-width: 1024px) {
+      font-size: 30px; 
+    }
+    &:hover {
+      color: #32E0C4;
+      cursor: pointer;
+    }
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;            
+  align-items: center;      
+`;
+
+const LoginButton = styled.button`
+  background: #000000;
+  color: #32e0c4;
+  padding: 5px 20px;
+  font-size: 18px;
+  font-weight: bold; 
+  border: 2px solid;
+  border-color: #32e0c4;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-left: 20px;       
+  transition: background 0.3s;
+
+  &:hover {
+    background: linear-gradient(to right, #393e46, #32e0c4);
+    color: #000000;
+    border: none;
+    padding: 6px 20px;
+  }
+`;
+
+const SignupButton = styled.button`
+  background: #32e0c4;
+  color: #000000;
+  padding: 6px 20px;
+  font-size: 18px;
+  font-weight: bold; 
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-left: 20px;       
+  transition: background 0.3s;
+
+  &:hover {
+    background: linear-gradient(to right, #393e46, #32e0c4);
+  }
+`;
 
 const NavBar = () => {
   return (
-    <div>
-        <h1>NavBar</h1>
-    </div>
-  )
+    <StyledNavbar>
+      <img src="../assets/LOGO_cropped.png" alt="Logo" />
+      <Nav>
+        <h2>Home</h2>
+        <h2>Product</h2>
+        <h2>Contact</h2>
+      </Nav>
+      <ButtonContainer>
+        <LoginButton type="button">LOG IN</LoginButton>
+        <SignupButton type="button">SIGN UP</SignupButton>
+      </ButtonContainer>
+    </StyledNavbar>
+  );
 }
 
 export default NavBar;
